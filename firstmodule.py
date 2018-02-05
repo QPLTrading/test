6b074c28939fc125c619c2b8290ff83c06df79d4
Eric <zheric@hotmail.com>
2018-02-05 13:38:01 +1100

    first version of firstmodule

---
+++ firstmodule.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 05:42:16 2017
+
+@author: USER
+"""
+print('firstmodule name is {}'.format(__name__))
+def main():
+    print('firstmodule name is {}'.format(__name__))
+
+
+if __name__ == '__main__':
+    main()
