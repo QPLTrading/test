b0ac8988021846fb5d8804afbe2489268f7bdd5b
Eric <zheric@hotmail.com>
2018-02-05 13:48:03 +1100

    second version of firstmodule

--- firstmodule.py
+++ firstmodule.py
@@ -5,6 +5,7 @@ Created on Thu Nov  2 05:42:16 2017
 @author: USER
 """
 print('firstmodule name is {}'.format(__name__))
+print('new line added')
 def main():
     print('firstmodule name is {}'.format(__name__))
 
